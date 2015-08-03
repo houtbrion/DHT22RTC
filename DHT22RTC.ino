049d78d3952b9add82dfa229a8a3266eaa42a44d
houtbrion <houtbrion@gmail.com>
2015-08-03 18:14:17 +0900

    skRTClibの更新への追従と，sleepをサポートしない機種への対応の追加．

--- DHT22RTC.ino
+++ DHT22RTC.ino
@@ -14,16 +14,16 @@
 *  1.00    2012-01-15  きむ茶工房(きむしげ)  Create                            *
 *******************************************************************************/
 
+//#define USE_SLEEP
 //
-// RTC8564用のライブラリを使うためのインクルードのロード
-//
-#include <Wire.h>
-#include <skRTClib.h>
+#define USE_DHT
+#define DHTPIN 5     // what pin we're connected to
 
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11 
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-
-#include <avr/sleep.h>
-//
 // RTCからarduinoに割込みを上げる端子と割込み番号の指定
 // 本当なら，割込み番号を入れたら端子も機種毎の切り替えを効かせたいが，とりあえず固定指定
 //
@@ -33,14 +33,20 @@
 // UNOはINT0,1が動作するため，こちらを利用
 #define INT_NUMBER 0
 #define PIN_NUMBER 2
+
+
 //
 //　端末が眠る場合の眠りの深さの指定
 //
+#ifdef USE_SLEEP
 //#define STANDBY_MODE SLEEP_MODE_IDLE
 //#define STANDBY_MODE SLEEP_MODE_ADC
 //#define STANDBY_MODE SLEEP_MODE_PWR_SAVE
 //#define STANDBY_MODE SLEEP_MODE_STANDBY
 #define STANDBY_MODE SLEEP_MODE_PWR_DOWN
+#else
+#define STANDBY_MODE 0
+#endif /* USE_SLEEP */
 //
 // 端末が眠る期間の指定
 //
@@ -50,19 +56,25 @@
 #define SLEEP_UNIT 2 //秒単位
 //#define SLEEP_UNIT 3 //分単位
 
+
+//
+// RTC8564用のライブラリを使うためのインクルードのロード
+//
+#include <Wire.h>
+#include <skRTClib.h>
+
+#ifdef USE_SLEEP
+#include <avr/sleep.h>
+#endif /* USE_SLEEP */
+
+
 #include <stdio.h>
 #include <DHT.h>
 
 /*
  * DHT関係の定義
  */
-#define USE_DHT
-#define DHTPIN 5     // what pin we're connected to
 
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11 
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
 // Connect pin 1 (on the left) of the sensor to +5V
 // NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
@@ -72,7 +84,7 @@
 // Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
 
 // Initialize DHT sensor for normal 16mhz Arduino
-DHT dht(DHTPIN, DHTTYPE);
+//DHT dht(DHTPIN, DHTTYPE);
 // NOTE: For working with a faster chip, like an Arduino Due or Teensy, you
 // might need to increase the threshold for cycle counts considered a 1 or 0.
 // You can do this by passing a 3rd parameter for this threshold.  It's a bit
@@ -81,6 +93,8 @@ DHT dht(DHTPIN, DHTTYPE);
 // Arduino Due that runs at 84mhz a value of 30 works.
 // Example to initialize DHT sensor for Arduino Due:
 //DHT dht(DHTPIN, DHTTYPE, 30);
+// arduino M0 pro
+DHT dht(DHTPIN, DHTTYPE,18);
 
 static FILE uartout;
 
@@ -97,7 +111,7 @@ static int uart_putchar (char c, FILE *stream) {
 ////////////////////////////////////////////////////////////////////////////////
 void InterRTC()
 {
-  RTC.InterFlag = 1 ;
+  skRTC.InterFlag = 1 ;
   Serial.println("Interupt!");
 }
 
@@ -113,7 +127,7 @@ void setup()
   stdout = &uartout;
   pinMode( PIN_NUMBER, INPUT);    // 後の項で使用
   Serial.begin(9600) ;                    // シリアル通信の初期化
-  ans = RTC.begin(PIN_NUMBER,INT_NUMBER,InterRTC,12,1,10,2,15,30,0) ;  // 2012/01/10 火 15:30:00 でRTCを初期化する
+  ans = skRTC.begin(PIN_NUMBER,INT_NUMBER,InterRTC,12,1,10,2,15,30,0) ;  // 2012/01/10 火 15:30:00 でRTCを初期化する
   if (ans == 0) {
     Serial.println("Successful initialization of the RTC") ;// 初期化成功
   } else {
@@ -121,7 +135,7 @@ void setup()
     Serial.println(ans) ;
     while(1) ;                         // 処理中断
   }
-  RTC.SetTimer(SLEEP_UNIT,SLEEP_DURATION) ;
+  skRTC.SetTimer(SLEEP_UNIT,SLEEP_DURATION) ;
 }
 /*******************************************************************************
 *  繰り返し実行される処理の関数(メインの処理)                                  *
@@ -131,12 +145,12 @@ void loop()
   byte tm[7] ; 
   char buf[24] ;
   goodNight(STANDBY_MODE);// 割込みが発生したか？
-  if (RTC.InterFlag == 1) {  // 割込みが発生したか？
-    RTC.rTime(tm) ;                    // RTCから現在の日付と時刻を読込む
-    RTC.cTime(tm,(byte *)buf) ;        // 日付と時刻を文字列に変換する
+  if (skRTC.InterFlag == 1) {  // 割込みが発生したか？
+    skRTC.rTime(tm) ;                    // RTCから現在の日付と時刻を読込む
+    skRTC.cTime(tm,(byte *)buf) ;        // 日付と時刻を文字列に変換する
     Serial.println(buf) ;              // シリアルモニターに表示
     if ((tm[1]==0x32)&&(tm[0]==0x00)){ // ３２分になったら機能終了
-      RTC.StopTimer() ;             // タイマー機能終了し無効にする
+      skRTC.StopTimer() ;             // タイマー機能終了し無効にする
     }
     float h = dht.readHumidity(); //湿度
     float t = dht.readTemperature(false); //摂氏
@@ -153,19 +167,21 @@ void loop()
       Serial.print("Heat Index : ");
       Serial.println(hi);
     }
-    RTC.InterFlag = 0 ;                // 割込みフラグをクリアする
+    skRTC.InterFlag = 0 ;                // 割込みフラグをクリアする
   }
 }
 
 void goodNight(int i) {
   Serial.println("  Good Night");
   delay(100);
+#ifdef USE_SLEEP
   noInterrupts();
   set_sleep_mode(i);
   sleep_enable();
   interrupts();
   sleep_cpu();
   sleep_disable();
+#endif /*USE_SLEEP*/
 }
 
 
